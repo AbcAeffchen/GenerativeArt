07df2ef9a79a3c227b64261b6cd9e81ac2cfe295
Alex Schickedanz <alex@ae.cs.uni-frankfurt.de>
2018-03-24 21:16:40 +0100

    Add modified CLI11 version.
    This version is modified to provide better help messages if a custom
    input takes more than one parameter.

---
+++ CLI11/CLI11Domain.h
@@ -0,0 +1,60 @@
+//
+// Created by Alex Schickedanz <alex@ae.cs.uni-frankfurt.de> on 15.03.18.
+//
+
+#ifndef GENERATIVEART_CLI11DOMAIN_H
+#define GENERATIVEART_CLI11DOMAIN_H
+
+#include <CLI11.hpp>
+#include <RandomFunction.h>
+#include <type_traits>
+
+template<typename T>
+constexpr std::string TypeName() {
+    if(std::is_same<T, int>::value)
+        return "INT";
+    else if(std::is_same<T, unsigned int>::value)
+        return "UINT";
+    else if(std::is_same<T, float>::value)
+        return "FLOAT";
+    else
+        return "";
+}
+
+template<typename T>
+CLI::Option* add_domain(CLI::App& app,
+                        std::string name,
+                        Domain<T>& variable,
+                        std::string description = "",
+                        const bool defaulted = false)
+{
+    CLI::callback_t reader = [&variable, name](CLI::results_t res)
+    {
+        T x, y;
+        bool worked = CLI::detail::lexical_cast(res[0], x) && CLI::detail::lexical_cast(res[1], y);
+
+        if(x > y)
+            throw CLI::ValidationError("The min value of \"" + name + " min max\" need to be smaller or equal to the max value, but " + std::to_string(x)
+                   + " is bigger than " + std::to_string(y) + ".");
+
+        if(worked)
+            variable = Domain<T>(x, y);
+
+        return worked;
+    };
+
+    CLI::Option* opt = app.add_option(std::move(name), reader, std::move(description), defaulted);
+    opt->set_custom_option(TypeName<T>() + "=" + std::to_string(static_cast<int>(variable.min))
+                           + " " +
+                           TypeName<T>() + "=" + std::to_string(static_cast<int>(variable.max)), 2, true);
+    if(defaulted)
+    {
+        std::stringstream out;
+        out << variable;
+        opt->set_default_str(out.str());
+    }
+
+    return opt;
+}
+
+#endif //GENERATIVEART_CLI11DOMAIN_H
