6d1a1da56979417199971031a7c101b71d08438b
Alex Schickedanz <alex@ae.cs.uni-frankfurt.de>
2018-03-24 22:43:24 +0100

    Add script to generate all images in a folder with a new resolution.

---
+++ regenerate_all.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Reads all png file names from a folder, creates a sub folder named after the
+# resolution and generates every image using the resolution.
+#
+# inputs:
+# 1. path to image folder
+# 2. target resolution
+
+#mkdir $1/$2 && (ls -1 $1 | grep .png) | xargs -i ./build/GenerativeArt -o $1/$2 -f {} -r $2
+if [ ! -d $1/$2 ]; then
+    mkdir $1/$2
+fi
+
+(ls -1 $1 | grep .png) | xargs -i ./build/GenerativeArt -o $1/$2/ -f {} -r $2
